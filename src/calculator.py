93ab400687bb320ed67b706bd12efe5d38771ab7
Juan Vergara <juanignaciovergara14@gmail.com>
2024-08-25 18:36:29 -0300

    Agrego operacion de division

---
+++ src/calculator.py
@@ -0,0 +1,12 @@
+from src import operations
+
+def calculate(a, b, operator):
+    match operator:
+        case '/':
+            return operations.divide(a, b)
+        case _:
+            raise ValueError(f"Operador invalido {operator}")
+
+
+
+
