fe8c720cba818bf2aef2c81ecfcf81c601904a04
LUCIANO AGUSTIN MACIAS <lumacias11702@gmail.com>
2024-08-27 21:44:21 -0300

    Resta agregada

--- src/calculator.py
+++ src/calculator.py
@@ -2,6 +2,8 @@ from src import operations
 
 def calculate(a, b, operator):
     match operator:
+        case '-':
+            return operations.subtract(a,b)
         case '/':
             return operations.divide(a, b)
         case _:
--- src/operations.py
+++ src/operations.py
@@ -1,3 +1,6 @@
 
 def divide(a, b):
     return a/ b
+
+def subtract(a,b):
+    return a-b
