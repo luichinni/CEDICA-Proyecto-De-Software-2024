51b4e3eb139918637a2e8562d7e4c711490bb0eb
ricciardinicolas75 <ricciardinicolas75@gmail.com>
2024-09-01 23:01:53 -0300

    template basico Home

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask import render_template
 
-def create_app(env="development", static_folder=""):
-    app = Flask(__name__)
+def create_app(env="development", static_folder="../../static"):
+    app = Flask(__name__, static_folder=static_folder)
+
     @app.route("/")
     def home():
-        return "Hola mundo!"
+        return render_template("home.html")
 
     return app
