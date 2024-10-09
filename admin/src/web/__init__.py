30ef264578e25dbaea933c428e524c44222804ca
Juan Vergara <juanignaciovergara14@gmail.com>
2024-10-09 18:22:15 -0300

    Registered employee blueprint in src/web/init.py

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -1,21 +1,20 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 from web.handlers import error
 from src.core import database
 from src.core.config import config
 
 from src.core.models.user import User
-from src.core.models.employee import Employee
+from src.core.models import Employee
 from src.core.models.user.permission import Permission
 from src.core.models.user.role_permission import RolePermission
 from src.core.models.user.role import Role
 
-from src.core.services.user_service import UserService 
-from src.core.services.role_service import RoleService 
-from src.core.services.employee_service import EmployeeService 
+from src.core.services.user_service import UserService, RoleService
+from src.core.services import employee_service
 from src.core.services.permission_service import PermissionService 
 
-from src.web.controllers.user_controller import bp as users_bp 
+from src.web.controllers.user_controller import bp as users_bp
+from src.web.controllers.employee.employee_controller import bp as employee_bp
 
 def create_app(env="development", static_folder="../../static"):
     app = Flask(__name__, static_folder=static_folder)
@@ -31,10 +30,11 @@ def create_app(env="development", static_folder="../../static"):
     app.register_error_handler(404, error.not_found_error)
 
     app.register_blueprint(users_bp) 
+    app.register_blueprint(employee_bp)
 
     @app.cli.command(name="reset-db")
     def reset_db():
         database.reset()
-        database.init(UserService, RoleService, EmployeeService, PermissionService)
+        database.init(UserService, RoleService, employee_service, PermissionService)
 
     return app
