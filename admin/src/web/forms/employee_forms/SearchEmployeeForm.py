c07a1e79b89234257cf7ae1c51a91c67fe0dcb05
Juan Vergara <juanignaciovergara14@gmail.com>
2024-10-15 00:40:32 -0300

    Add form for employees search feature

---
+++ admin/src/web/forms/employee_forms/SearchEmployeeForm.py
@@ -0,0 +1,20 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, SubmitField, BooleanField
+from wtforms.validators import Optional
+from src.core.enums.employee_enum.PuestoLaboralEnum import PuestoLaboralEnum
+
+class SearchEmployeeForm(FlaskForm):
+    nombre = StringField('Nombre', validators=[Optional()])
+    apellido = StringField('Apellido', validators=[Optional()])
+    dni = StringField('DNI', validators=[Optional()])
+    email = StringField('Email', validators=[Optional()])
+    puesto_laboral = SelectField('Puesto Laboral', choices=[
+        (puesto_laboral.name, puesto_laboral.value) for puesto_laboral in PuestoLaboralEnum], validators=[Optional()])
+    order_by = SelectField('Ordenar por', choices=[
+        ('nombre', 'Nombre'),
+        ('apellido', 'Apellido'),
+        ('created_at', 'Fecha de creación')
+    ], validators=[Optional()])
+
+    ascending = BooleanField('Orden ascendente', default=True)
+    submit = SubmitField('Buscar')
