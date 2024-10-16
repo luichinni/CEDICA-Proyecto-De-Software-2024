564c1b913459d0cfed4b4dd201bf14ef1ac923c3
LUCIANO AGUSTIN MACIAS <lumacias11702@gmail.com>
2024-10-15 23:14:09 -0300

    Environ para vault

--- admin/src/core/config.py
+++ admin/src/core/config.py
@@ -1,14 +1,21 @@
 import os
 from dotenv import load_dotenv
+
 load_dotenv(dotenv_path='.env')
+
 class Config(object):
     TESTING = False
-    SECRET_KEY= "my_precious"
+    SECRET_KEY= os.environ.get('SECRET_KEY_PASS') if os.environ.get('SECRET_KEY_PASS') is not None else 'my_precious' # "my_precious"
     SESSION_TYPE= "filesystem"
     SESSION_PERMANENT= False
 
 class ProductionConfig(Config):
-    pass
+    DB_USER = os.environ.get('DATABASE_USERNAME')
+    DB_PASSWORD = os.environ.get('DATABASE_PASSWORD')
+    DB_HOST = os.environ.get('DATABASE_HOST')
+    DB_PORT = os.environ.get('DATABASE_PORT')
+    DB_NAME = os.environ.get('DATABASE_NAME')
+    SQLALCHEMY_DATABASE_URI = (f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
 
 class DevelopmentConfig(Config):
     DB_USER = os.getenv('DB_USER')
