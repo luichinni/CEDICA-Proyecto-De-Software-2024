a0f526aa64f340b124330bd5ac6b43c25aec1712
Juan Vergara <juanignaciovergara14@gmail.com>
2024-10-23 20:09:44 -0300

    Rename payment bp to 'payment'

--- admin/src/web/controllers/payment_controller.py
+++ admin/src/web/controllers/payment_controller.py
@@ -7,7 +7,7 @@ from src.web.forms.payment_forms.SearchPaymentForm import SearchPaymentForm
 from src.web.handlers.auth import check_permissions
 from web.handlers import handle_error, get_int_param
 
-bp = Blueprint('payment_controller', __name__, url_prefix='/payments')
+bp = Blueprint('payment', __name__, url_prefix='/payments')
 
 #TODO: ADD 'SHOW' FEATURE AND CHECK PERMISSIONS
 
@@ -27,7 +27,7 @@ def index():
 #TODO: VER COMO HAGO PAGINACION ACA
 @bp.route('/search', methods=['GET'])
 @check_permissions(f"{PermissionModel.PAYMENT.value}_{PermissionCategory.INDEX.value}")
-@handle_error(lambda: url_for('payment_controller.index'))
+@handle_error(lambda: url_for('payment.index'))
 def search():
     form = SearchPaymentForm()
     payments = []
@@ -55,20 +55,20 @@ def create_payment():
         }
         PaymentService.create_payment(new_payment)
         flash('Pago registrado exitosamente', 'success')
-        return redirect(url_for('payment_controller.index'))
+        return redirect(url_for('payment.index'))
     return render_template('payment/create.html', form=form)
 
 @bp.route('/delete/<int:payment_id>', methods=['POST'])
 @check_permissions(f"{PermissionModel.PAYMENT.value}_{PermissionCategory.DESTROY}")
-@handle_error(lambda: url_for('payment_controller.index'))
+@handle_error(lambda: url_for('payment.index'))
 def delete_payment(payment_id):
     payment = PaymentService.get_payment_by_id(payment_id)
     flash('Pago eliminado exitosamente', 'success')
-    return redirect(url_for('payment_controller.index'))
+    return redirect(url_for('payment.index'))
 
 @bp.route('/edit/<int:payment_id>', methods=['GET', 'POST'])
 @check_permissions(f"{PermissionModel.PAYMENT.value}_{PermissionCategory.UPDATE}")
-@handle_error(lambda: url_for('payment_controller.index'))
+@handle_error(lambda: url_for('payment.index'))
 def update_payment(payment_id):
     payment = PaymentService.get_payment_by_id(payment_id)
     form = PaymentForm()
@@ -82,6 +82,6 @@ def update_payment(payment_id):
         }
         PaymentService.update_payment(payment_id, updated_payment)
         flash('Pago actualizado exitosamente', 'success')
-        return redirect(url_for('payment_controller.index'))
+        return redirect(url_for('payment.index'))
     return render_template('payment/edit.html', form=form, payment=payment)
 
